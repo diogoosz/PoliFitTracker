bf997ac576cb07f490f23045c0285e483f3b1b3b
Diogo Marciano <diogo.costa@poli.digital>
2025-11-05 17:27:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4662689289-857d9",
+  "appId": "1:862395535961:web:5985211c3631f8fea3d748",
+  "apiKey": "AIzaSyDQK8X-ujxyfwag6nKuAAThdnI4bDKG0bw",
+  "authDomain": "studio-4662689289-857d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "862395535961"
+};
